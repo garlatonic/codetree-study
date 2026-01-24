9f1dad8a6fcbf26f310973250f6936d4c7d53f1b
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-01-24 13:25:23 +0900

    Title: 정수의 최솟값, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260124/정수의 최솟값/minimum-value-of-an-integer.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [a, b, c] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+const func = (a, b, c) => {
+    return Math.min(a, b, c);
+}
+
+console.log(func(a, b, c))
