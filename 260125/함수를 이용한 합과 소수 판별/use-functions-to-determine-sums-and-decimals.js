679230286f3fb961aed7175205c069351c18a345
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-01-25 11:50:25 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260125/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.js
+++ 260125/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.js
@@ -4,14 +4,20 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [A, B] = input[0].split(" ").map(Number);
 
 // Please Write your code here.
-function isAllEven(num) {
-    const numStr = num.toString().split("");
-    return numStr.length === numStr.filter((num) => num * 1 !== 0 && num % 2 === 0).length;
+function isSumEven(num) {
+    const numArr = num.toString().split("").map(Number);
+    return numArr.reduce((acc, cur) => acc += cur, 0) % 2 === 0;
+}
+function isSosu(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
 }
 
 let count = 0;
 for (let i = A; i <= B; i++) {
-    if (isAllEven(i)) {
+    if (isSumEven(i) && isSosu(i)) {
         count++;
     }
 }
