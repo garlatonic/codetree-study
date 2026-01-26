0502b104af5e193463e6f42239913c8d9e93fb62
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-01-26 22:05:15 +0900

    Title: 블럭쌓는 명령2, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260126/블럭쌓는 명령2/block-stacking-commands2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const segments = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
+
+// Please write your code here
+
+const arr = Array.from({ length: n }).fill(0);
+for (const segment of segments) {
+    const [s, e] = segment;
+
+    for (let i = s - 1; i < e; i++) {
+        arr[i]++;
+    }
+}
+
+console.log(Math.max(...arr))
