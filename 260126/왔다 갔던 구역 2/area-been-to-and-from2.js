2fcf75dda36e826823883b8d148bed3ffb443aa0
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-01-26 23:58:50 +0900

    Title: 왔다 갔던 구역 2, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260126/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 260126/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -5,7 +5,7 @@ const n = Number(input[0]);
 const commands = input.slice(1, n + 1);
 
 // Please Write your code here.
-const MAX_OFFSET = 101;
+const MAX_OFFSET = n * 10 * 2 + 1;
 const arr = Array(MAX_OFFSET).fill(0);
 
 let current = Math.floor(MAX_OFFSET / 2) + 1;
@@ -13,14 +13,13 @@ for (const command of commands) {
     const [m, d] = command.split(" ");
     if (d === "L") {
         for (let i = 0; i < Number(m); i++) {
-            arr[current + i] += 1
+            arr[current - i - 1] += 1;
         }
     } else {
         for (let i = 0; i < Number(m); i++) {
-            arr[current - i] += 1
+            arr[current + i] += 1;
         }
     }
-    current = d === "L" ? current - Number(m) : current + Number(m)
+    current = d === "L" ? current - Number(m) : current + Number(m);
 }
-
 console.log(arr.filter((el) => el > 1).length);
