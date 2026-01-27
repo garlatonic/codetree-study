eaa511a94b37d57daa5fb36a02fe6117b32548ef
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-01-27 22:51:56 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260127/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
+++ 260127/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
@@ -19,22 +19,35 @@ developers[p - 1].infested = true;
 let time = 0;
 let index = 0;
 
+shakes.sort((a, b) => a.time - b.time);
+
 while (index < t) {
     time += shakes[index].time;
 
     const person1 = shakes[index].person1 - 1;
     const person2 = shakes[index].person2 - 1;
 
-    if (developers[person1].count > 0) {
+    // 악수하기 전 두 사람의 상태를 먼저 확인
+    const isP1Infected = developers[person1].infested;
+    const isP2Infected = developers[person2].infested;
+
+    // [사건 1] P1이 감염자이고 전파 능력이 있다면 -> P1은 악수 횟수를 쓰고 P2에게 영향을 줌
+    if (isP1Infected && developers[person1].count > 0) {
         developers[person1].count--;
-        developers[person2].infested = true;
+        developers[person2].infested = true; // P2가 원래 감염자였든 아니든 상관없이 감염 상태로 둠
+    }
+
+    // [사건 2] P2가 감염자이고 전파 능력이 있다면 -> P2는 악수 횟수를 쓰고 P1에게 영향을 줌
+    if (isP2Infected && developers[person2].count > 0) {
+        developers[person2].count--;
+        developers[person1].infested = true;
     }
 
     index++;
 }
 
 const answer = [];
-Object.entries(developers).forEach(([k, v]) => {
+Object.entries(developers).forEach(([_, v]) => {
     if (v.infested) answer.push(1);
     else answer.push(0);
 })
