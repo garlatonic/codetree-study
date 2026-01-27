e3cf8e8b74e7952cffc24bb060b07bddba99875d
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-01-27 17:52:05 +0900

    Title: 연속되는 수 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260127/연속되는 수 2/continuous-number2.js
+++ 260127/연속되는 수 2/continuous-number2.js
@@ -5,10 +5,18 @@ const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
 // Please Write your code here.
-let count = 0;
+let count = 1;
+let maxCount = 1;
 
-for (let i = 0; i < arr.length; i++) {
-    if (i === 0 || arr[i] !== arr[i - 1]) count++
+for (let i = 1; i < n; i++) {
+  if (arr[i] === arr[i - 1]) {
+    count++;
+  } else {
+    maxCount = Math.max(maxCount, count);
+    count = 1;
+  }
 }
 
-console.log(count)
+maxCount = Math.max(maxCount, count);
+
+console.log(maxCount);
