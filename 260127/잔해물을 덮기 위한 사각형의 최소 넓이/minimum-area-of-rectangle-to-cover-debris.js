348e7e1872f1f7520c74218612d8c24572da335b
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-01-27 17:13:18 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 170ms, Memory: 59MB, Status: Passed - Codetree

--- 260127/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 260127/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -25,26 +25,32 @@ getArea(rect1, "+");
 getArea(rect2, "-");
 
 
-let [mx1, my1, mx2, my2] = [0, 0, 0, 0];
+// 잔해물이 남아있지 않는지 확인
+if (area.every((row) => row.every((cell) => cell === 0))) {
+    console.log(0);
+} else {
+    // 남아있는 잔해물이 있을 때만 넓이 계산
+    let [mx1, my1, mx2, my2] = [0, 0, 0, 0];
 
-// y축 최소값 찾기
-for (let y = 0; y < MAX_R; y++) {
-  if (area[y].indexOf(1) !== -1) {
-    my1 = y;
-    break;
-  }
-}
-
-// x축 최소값, y축 최대값, x축 최대값 찾기
-for (let y = 0; y < MAX_R; y++) {
-  for (let x = 0; x < MAX_R; x++) {
-    if (area[y][x] === 1) {
-      if (mx1 === 0 || x < mx1) mx1 = x;
-      if (mx2 === 0 || x > mx2) mx2 = x;
-      if (my2 === 0 || y > my2) my2 = y;
+    // y축 최소값 찾기
+    for (let y = 0; y < MAX_R; y++) {
+        if (area[y].indexOf(1) !== -1) {
+            my1 = y;
+            break;
+        }
     }
-  }
-}
 
-const result = (mx2 - mx1 + 1) * (my2 - my1 + 1);
-console.log(result);
+    // x축 최소값, y축 최대값, x축 최대값 찾기
+    for (let y = 0; y < MAX_R; y++) {
+        for (let x = 0; x < MAX_R; x++) {
+            if (area[y][x] === 1) {
+                if (mx1 === 0 || x < mx1) mx1 = x;
+                if (mx2 === 0 || x > mx2) mx2 = x;
+                if (my2 === 0 || y > my2) my2 = y;
+            }
+        }
+    }
+
+    const result = (mx2 - mx1 + 1) * (my2 - my1 + 1);
+    console.log(result);
+}
