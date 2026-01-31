96a3421b627a99f46b4762ffbde5107dd94e10b9
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-01 00:43:46 +0900

    Title: 이상한 진수 2, Time: 91ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260201/이상한 진수 2/awkward-digits-2.js
+++ 260201/이상한 진수 2/awkward-digits-2.js
@@ -4,13 +4,15 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const a = input[0];
 
 // Please Write your code here.
-let max = parseInt(a, 2);
+let max = 0;
 
-for(let i = 0; i < a.length; i++) {
+for (let i = 0; i < a.length; i++) {
     let temp = [...a];
     temp[i] = 1;
 
-    max = Math.max(parseInt(temp.join(""), 2), max);
+    if (temp.join("") !== a) {
+        max = Math.max(parseInt(temp.join(""), 2), max);
+    }
 }
 
 console.log(max)
