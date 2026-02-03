9027ab77bbf1bbc85fb21ba7400a7f76fa67cc1b
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-03 21:39:14 +0900

    Title: 밭의 높이를 고르게하기, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260203/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, h, t] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+let minCost = Infinity;
+for (let i = 0; i < n - t; i++) {
+    let cost = 0;
+    for (let j = i; j < i + t; j++) {
+        cost += Math.abs(h - arr[j]);
+    }
+
+    minCost = Math.min(minCost, cost);
+}
+
+console.log(minCost);
