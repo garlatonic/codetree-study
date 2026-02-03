362d71a4e37769433348a492ec69db703e50a88e
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-03 20:26:28 +0900

    Title: 특정 구간의 원소 평균값, Time: 72ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260203/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].trim().split(' ').map(Number);
+
+// Please Write your code here.
+let count = 0;
+for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+        let sum = 0;
+        for (let k = i; k < j + 1; k++) {
+            sum += arr[k];
+        }
+
+        const average = sum / j;
+        if(arr.includes(average)) count++;
+    }
+}
+
+console.log(count);
