d1898c90438cbf8f3798b52e144469789ac89958
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-04 18:17:26 +0900

    Title: 삼각형 만들기, Time: 66ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 260204/삼각형 만들기/create-triangle.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const points = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+// Please Write your code here.
+
+let maxArea = 0;
+for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
+            const [x1, y1] = points[i];
+            const [x2, y2] = points[j];
+            const [x3, y3] = points[k];
+
+            let area = 0;
+            if (x1 === x2 && y1 === y3) {
+                area = Math.abs((y2 - y1) * (x3 - x1));
+            } else if (x1 === x3 && y1 === y2) {
+                area = Math.abs((y3 - y1) * (x2 - x1));
+            } else if (x2 === x3 && y2 === y1) {
+                area = Math.abs((y3 - y2) * (x2 - x1));
+            }
+
+            maxArea = Math.max(maxArea, area)
+        }
+    }
+}
+
+console.log(maxArea)
