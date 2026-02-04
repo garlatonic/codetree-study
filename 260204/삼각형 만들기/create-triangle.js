5b52b0940f59ad98149b8798db207eff26c240c0
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-04 18:25:41 +0900

    Title: 삼각형 만들기, Time: 129ms, Memory: 14MB, Status: Passed - Codetree

--- 260204/삼각형 만들기/create-triangle.js
+++ 260204/삼각형 만들기/create-triangle.js
@@ -12,13 +12,26 @@ for (let i = 0; i < n; i++) {
             const [x2, y2] = points[j];
             const [x3, y3] = points[k];
 
+            // x좌표가 같은 쌍이 있는지
+            const xMatch = (x1 === x2) || (x1 === x3) || (x2 === x3);
+            // y좌표가 같은 쌍이 있는지  
+            const yMatch = (y1 === y2) || (y1 === y3) || (y2 === y3);
+
+            if (!xMatch && !yMatch) continue;
+
             let area = 0;
             if (x1 === x2 && y1 === y3) {
                 area = Math.abs((y2 - y1) * (x3 - x1));
+            } else if (x1 === x2 && y2 === y3) {
+                area = Math.abs((y2 - y1) * (x3 - x2));
             } else if (x1 === x3 && y1 === y2) {
                 area = Math.abs((y3 - y1) * (x2 - x1));
-            } else if (x2 === x3 && y2 === y1) {
+            } else if (x1 === x3 && y2 === y3) {
+                area = Math.abs((y3 - y1) * (x3 - x2));
+            } else if (x2 === x3 && y1 === y2) {
                 area = Math.abs((y3 - y2) * (x2 - x1));
+            } else if (x2 === x3 && y1 === y3) {
+                area = Math.abs((y3 - y2) * (x3 - x1));
             }
 
             maxArea = Math.max(maxArea, area)
