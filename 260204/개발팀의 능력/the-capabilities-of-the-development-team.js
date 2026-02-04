51b9ef566ebcc0be0654a7167a8e4716bbb56209
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-04 16:43:19 +0900

    Title: 개발팀의 능력, Time: 64ms, Memory: 11MB, Status: Passed - Codetree

--- 260204/개발팀의 능력/the-capabilities-of-the-development-team.js
+++ 260204/개발팀의 능력/the-capabilities-of-the-development-team.js
@@ -7,18 +7,16 @@ const abilities = input[0].split(" ").map(Number);
 
 // 최대 능력의 팀과 최소 능력의 팀 간의 능력 차이가 최소가 되도록 만드는 프로그램을 작성
 function calcDiff(i, j, k, l) {
-    if(abilities[i] === abilities[j] || abilities[k] === abilities[l]) return -1;
-
     const sum1 = abilities[i] + abilities[j];
     const sum2 = abilities[k] + abilities[l];
     const sum3 = abilities.reduce((acc, cur) => acc + cur, 0) - sum1 - sum2;
 
-    // 능력 차이가 최소가 되도록
-    let diff = Math.abs(sum1 - sum2);
-    diff = Math.max(diff, Math.abs(sum1 - sum3));
-    diff = Math.max(diff, Math.abs(sum2 - sum3));
+    if (sum1 === sum2 || sum2 === sum3 || sum1 === sum3) return Infinity;
 
-    return diff;
+    const max = Math.max(sum1, sum2, sum3);
+    const min = Math.min(sum1, sum2, sum3);
+
+    return max - min;
 }
 
 // 여기까지가 첫번째 팀원
@@ -26,7 +24,7 @@ let minDiff = Infinity;
 for (let i = 0; i < 5; i++) {
     for (let j = i + 1; j < 5; j++) {
         // 여기서부터 두번째 팀원
-        for (let k = j + 1; k < 5; k++) {
+        for (let k = 0; k < 5; k++) {
             for (let l = k + 1; l < 5; l++) {
                 if (i === k || i === l || j === k || j === l) continue;
 
@@ -36,4 +34,4 @@ for (let i = 0; i < 5; i++) {
     }
 }
 
-console.log(minDiff);
+console.log(minDiff === Infinity ? -1 : minDiff);
