f1dfd759f72d570ca559b382f4163aa10c10fb7e
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-05 16:30:27 +0900

    Title: 이상한 폭탄 2, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260205/이상한 폭탄 2/strange-bomb-2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const nums = input.slice(1, n + 1).map(Number);
+
+// Please Write your code here.
+let maxBomb = -1;
+for (let i = 0; i < n; i++) {
+    let bomb = 0;
+
+    for (let j = 0; j < n; j++) {
+        if (i === j) continue;
+
+        if(nums[i] === nums[j] && j - i < k) bomb = nums[i];
+    }
+
+    maxBomb = Math.max(maxBomb, bomb)
+}
+
+console.log(maxBomb)
