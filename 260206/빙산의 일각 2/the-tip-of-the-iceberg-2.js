f64e6a15b46dc72252ad554fec52bc38963b178a
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-06 23:37:55 +0900

    Title: 빙산의 일각 2, Time: 70ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260206/빙산의 일각 2/the-tip-of-the-iceberg-2.js
+++ 260206/빙산의 일각 2/the-tip-of-the-iceberg-2.js
@@ -7,17 +7,13 @@ const h = input.slice(1, 1 + n).map(Number);
 // Please Write your code here.
 let maxCount = 0;
 for (let i = 1; i <= 1000; i++) {
-    let isConnected = false;
-    let count = 1;
+    let count = h[0] > i ? 1 : 0;
 
     for (let j = 1; j < n; j++) {
         const prev = h[j - 1];
         const curr = h[j];
 
-        if (prev > i && curr > i) {
-            isConnected = true;
-        } else if (prev > i && curr <= i) {
-            isConnected = false;
+        if (prev > i && curr <= i) {
             count++;
         }
     }
