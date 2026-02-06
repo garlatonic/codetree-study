d412c15248347c30c28e9451098490b75383879d
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-07 00:06:56 +0900

    Title: 야바위, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260207/야바위/ya-rock.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const commands = input.slice(1).map(line => line.split(' ').map(Number));
+// Please Write your code here.
+
+let maxCount = 0;
+let maxCup = 0;
+for (let i = 1; i <= 3; i++) {
+    let cup = [1, 2, 3];
+    let count = 0;
+
+    for (let j = 0; j < n; j++) {
+        const [a, b, c] = commands[j];
+
+        [cup[a - 1], cup[b - 1]] = [cup[b - 1], cup[a - 1]];
+        if (cup[c - 1] === i) count++;
+    }
+
+    if (maxCount < count) maxCup = i;
+    maxCount = Math.max(maxCount, count);
+}
+
+console.log(maxCup)
