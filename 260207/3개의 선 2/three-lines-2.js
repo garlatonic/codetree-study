55f03d2ccf2fe19d4260385a19ad48dc88236b50
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-07 02:45:12 +0900

    Title: 3개의 선 2, Time: 71ms, Memory: 12MB, Status: Passed - Codetree

--- 260207/3개의 선 2/three-lines-2.js
+++ 260207/3개의 선 2/three-lines-2.js
@@ -7,11 +7,12 @@ const points = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 // Please Write your code here.
 let through = false;
 
+// Case 1 : x축 평행만 3개
 for (let a = 0; a <= 10; a++) {
     for (let b = 0; b <= 10; b++) {
         for (let c = 0; c <= 10; c++) {
             const result = points.every(([x, y]) => {
-                return (x === a || x === b || x === c) || (x === a || x === b || y === c) || (x === a || y === b || y === c) || (y === a || y === b || y === c);
+                return x === a || x === b || x === c;
             });
 
             if (result) {
@@ -23,5 +24,61 @@ for (let a = 0; a <= 10; a++) {
     }
     if (through) break;
 }
+
+// Case 2 : x축 평행 2개, y축 평행 1개
+for (let a = 0; a <= 10; a++) {
+    for (let b = 0; b <= 10; b++) {
+        for (let c = 0; c <= 10; c++) {
+            const result = points.every(([x, y]) => {
+                return x === a || x === b || y === c;
+            });
+
+            if (result) {
+                through = true;
+                break;
+            }
+        }
+        if (through) break;
+    }
+    if (through) break;
+}
+
+
+// Case 3 : x축 평행 1개, y축 평행 2개
+for (let a = 0; a <= 10; a++) {
+    for (let b = 0; b <= 10; b++) {
+        for (let c = 0; c <= 10; c++) {
+            const result = points.every(([x, y]) => {
+                return x === a || y === b || y === c;
+            });
+
+            if (result) {
+                through = true;
+                break;
+            }
+        }
+        if (through) break;
+    }
+    if (through) break;
+}
+
+// Case 4 : y축 평행만 3개
+for (let a = 0; a <= 10; a++) {
+    for (let b = 0; b <= 10; b++) {
+        for (let c = 0; c <= 10; c++) {
+            const result = points.every(([x, y]) => {
+                return y === a || y === b || y === c;
+            });
+
+            if (result) {
+                through = true;
+                break;
+            }
+        }
+        if (through) break;
+    }
+    if (through) break;
+}
+
 
 console.log(through ? 1 : 0);
