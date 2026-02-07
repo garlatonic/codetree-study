a82606daf947b03d906c55d153ed592931b8cf9e
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-07 23:07:50 +0900

    Title: 팀으로 하는 틱택토 2, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 260207/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ 260207/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -7,6 +7,7 @@ let count = 0;
 for (let a = 1; a < 10; a++) {
     for (let b = a + 1; b < 10; b++) {
         if (a === b) continue;
+        let finish = false;
 
         // x축 방향으로 있는지?
         for (let y = 0; y < 3; y++) {
@@ -23,6 +24,8 @@ for (let a = 1; a < 10; a++) {
             }
         }
 
+        if (finish) continue;
+
         // y축 방향
         for (let x = 0; x < 3; x++) {
             const setY = new Set();
@@ -32,9 +35,12 @@ for (let a = 1; a < 10; a++) {
             if (setY.size === 2 && setY.has(a) && setY.has(b)) {
                 // console.log(`Found: a=${a}, b=${b}, direction=y`);
                 count++;
+                finish = true;
                 break;
             }
         }
+
+        if (finish) continue;
 
         // 좌상단에서 우하단으로 내려가는 대각선
         const setB = new Set();
@@ -45,8 +51,10 @@ for (let a = 1; a < 10; a++) {
         if (setB.size === 2 && setB.has(a) && setB.has(b)) {
             // console.log(`Found: a=${a}, b=${b}, direction=lt to rb`);
             count++;
-            break;
+            finish = true;
         }
+
+        if (finish) continue;
 
         // 좌하단에서 우상단으로 올라가는 대각선
         const setT = new Set();
@@ -57,7 +65,6 @@ for (let a = 1; a < 10; a++) {
         if (setT.size === 2 && setT.has(a) && setT.has(b)) {
             // console.log(`Found: a=${a}, b=${b}, direction=lb to rt`);
             count++;
-            break;
         }
     }
 }
