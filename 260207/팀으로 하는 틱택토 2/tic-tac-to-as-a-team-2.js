ee6b8025afa52a35ca3550fa3159440d789cd40c
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-07 23:01:33 +0900

    Title: 팀으로 하는 틱택토 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260207/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ 260207/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -8,19 +8,20 @@ for (let a = 1; a < 10; a++) {
     for (let b = a + 1; b < 10; b++) {
         if (a === b) continue;
 
-        let finish = false;
-
         // x축 방향으로 있는지?
         for (let y = 0; y < 3; y++) {
-            const result = board[y].every((el) => el === a || el === b);
+            const setX = new Set();
+            for (let x = 0; x < 3; x++) {
+                setX.add(board[y][x]);
+            }
 
-            if (result) {
+            if (setX.size === 2 && setX.has(a) && setX.has(b)) {
+                // console.log(`Found: a=${a}, b=${b}, direction=x`);
                 count++;
                 finish = true;
                 break;
             }
         }
-        if (finish) break;
 
         // y축 방향
         for (let x = 0; x < 3; x++) {
@@ -29,12 +30,11 @@ for (let a = 1; a < 10; a++) {
                 setY.add(board[y][x])
             }
             if (setY.size === 2 && setY.has(a) && setY.has(b)) {
+                // console.log(`Found: a=${a}, b=${b}, direction=y`);
                 count++;
-                finish = true;
                 break;
             }
         }
-        if (finish) break;
 
         // 좌상단에서 우하단으로 내려가는 대각선
         const setB = new Set();
@@ -43,11 +43,10 @@ for (let a = 1; a < 10; a++) {
             setB.add(board[y][x]);
         }
         if (setB.size === 2 && setB.has(a) && setB.has(b)) {
+            // console.log(`Found: a=${a}, b=${b}, direction=lt to rb`);
             count++;
-            finish = true;
             break;
         }
-        if (finish) break;
 
         // 좌하단에서 우상단으로 올라가는 대각선
         const setT = new Set();
@@ -56,11 +55,10 @@ for (let a = 1; a < 10; a++) {
             setT.add(board[y][x]);
         }
         if (setT.size === 2 && setT.has(a) && setT.has(b)) {
+            // console.log(`Found: a=${a}, b=${b}, direction=lb to rt`);
             count++;
-            finish = true;
             break;
         }
-        if (finish) break;
     }
 }
 
