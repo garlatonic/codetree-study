6b04d47fc92f33496377831903a9a0e52cf45825
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-08 00:01:00 +0900

    Title: 원소 값들의 최대 합, Time: 62ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260208/원소 값들의 최대 합/maximum-sum-of-element-values.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const arr = [0].concat(input[1].trim().split(' ').map(Number));
+
+// Please Write your code here.
+let maxSum = 0;
+for(let i = 0; i < n; i++) {
+    // i번째에 있는 원소가 시작 위치
+    let count = 1;
+    let move = arr[i];
+    const moveArr = [move];
+
+    while(count < m) {
+        move = arr[move];
+        moveArr.push(move);
+        count++;
+    }
+
+    maxSum = Math.max(maxSum, moveArr.reduce((acc, cur) => acc + cur, 0));
+}
+
+console.log(maxSum)
