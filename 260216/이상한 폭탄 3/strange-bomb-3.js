841ef34f4c55220c143ab61b224ba48f050bab2d
상아영혼방패 <62371174+garlatonic@users.noreply.github.com>
2026-02-16 22:33:23 +0900

    Title: 이상한 폭탄 3, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260216/이상한 폭탄 3/strange-bomb-3.js
+++ 260216/이상한 폭탄 3/strange-bomb-3.js
@@ -8,45 +8,43 @@ for (let i = 1; i <= n; i++) {
 }
 
 // Please write your code here.
+let maxCount = 0;
 let maxBomb = 0;
-let bomb = 0;
-for (let i = 0; i < n; i++) {
-    // i번째에 있는 폭탄
-    const bombNum = nums[i];
-    // 현재까지 터진 폭탄 갯수
+
+const numsSet = new Set(nums);
+
+numsSet.forEach((num) => {
+    let curIdx = nums.indexOf(num);
+    let prevBomb = nums.indexOf(num);
     let totalCount = 0;
-    // 지금 탐색하고있는 인덱스
-    let curr = i;
+    let count = 0;
 
-    while (curr < n) {
-        let count = 0;
-        let found = false;
+    while (curIdx < n) {
+        curIdx += 1;
 
-        for (let j = 0; j < k; j++) {
-            curr++;
-            if (curr >= n) break;
-
-            if (nums[curr] === bombNum) {
-                found = true;
-                count++;
-                break;
+        if (nums[curIdx] === num) {
+            if (curIdx - prevBomb <= k) { // 만약 k 거리 이내일 경우
+                if (count === 0) count = 1;
+                count += 1;
+            } else {
+                totalCount += count;
+                count = 0;
             }
+
+            prevBomb = curIdx;
         }
-
-        if (found) count++;
-
-        totalCount += count;
     }
 
-    if (maxBomb < totalCount) {
-        bomb = bombNum;
-        maxBomb = totalCount;
-    } else if (totalCount === 0) {
-        continue;
-    } else if (maxBomb === totalCount) {
-        bomb = Math.max(bomb, bombNum);
-    }
-}
+    totalCount += count;
 
-console.log(bomb);
-process.exit();
+    if (totalCount > 0) {
+        if (totalCount > maxCount) {
+            maxCount = totalCount;
+            maxBomb = num;
+        } else if (totalCount === maxCount && maxBomb < num) {
+            maxBomb = num;
+        }
+    }
+})
+
+console.log(maxBomb)
