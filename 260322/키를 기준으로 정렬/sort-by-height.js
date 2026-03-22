b6cfd293ba3c662ebb21d2be24fe12a72e3e0362
IvoryShield <62371174+garlatonic@users.noreply.github.com>
2026-03-22 16:10:53 +0900

    Title: 키를 기준으로 정렬, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260322/키를 기준으로 정렬/sort-by-height.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const studentsInput = [];
+for (let i = 1; i <= n; i++) {
+    studentsInput.push(input[i].split(' '));
+}
+// Please Write your code here.
+studentsInput.sort((a, b) => a[1] - b[1]);
+console.log(studentsInput.map((el) => el.join(" ")).join("\n"));
