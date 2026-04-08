8f0e8923f1f3c546243584ce7556a41f4a1da022
IVORY <62371174+garlatonic@users.noreply.github.com>
2026-04-08 12:51:45 +0900

    Title: 벽이 있는 충돌 실험, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/벽이 있는 충돌 실험/collision-experiment-with-wall.js
+++ 260408/벽이 있는 충돌 실험/collision-experiment-with-wall.js
@@ -15,7 +15,7 @@ for (let i = 0; i < t; i++) {
 
     const board = Array.from({ length: n }, () => Array(n).fill(""));
     for (const [x, y, d] of marbles) {
-        board[y][x] = d;
+        board[x][y] = d;
     }
 
     const dy = [1, -1, 0, 0];
@@ -30,29 +30,31 @@ for (let i = 0; i < t; i++) {
 
     // 아주 오래된 시간이 흐른 뒤에도 구슬이 안 부딪힐 경우에만 멈추도록 한다
     let time = 0;
-    while (time < n * n) {
+    const LIMIT = 2 * n;
+    while (time < LIMIT) {
         let isBlocked = false;
 
         const newBoard = Array.from({ length: n }, () => Array(n).fill(""));
         for (let r = 0; r < n; r++) {
             for (let c = 0; c < n; c++) {
-                if (!board[r][c]) continue;
+                const ch = board[r][c]
+                if (!ch) continue;
 
-                const d = dir[board[r][c]];
+                const d = dir[ch];
                 const ny = r + dy[d];
                 const nx = c + dx[d];
 
-                // 구슬이 보드 밖으로 나갈 경우 방향을 반대로 바꿔준다
+                // 구슬이 벽에 부딪히면 움직이지 않고 움직이는 방향만 반대로 뒤집힌다.
                 if (!inRange(ny, nx, n)) {
-                    if (board[r][c] === "U") newBoard[r][c] = "D";
-                    else if (board[r][c] === "D") newBoard[r][c] = "U";
-                    else if (board[r][c] === "R") newBoard[r][c] = "L";
-                    else newBoard[r][c] = "R";
+                    if (ch === "U") newBoard[r][c] += "D";
+                    else if (ch === "D") newBoard[r][c] += "U";
+                    else if (ch === "R") newBoard[r][c] += "L";
+                    else newBoard[r][c] += "R";
                     continue;
                 }
 
-                // 여러개가 부딪힐 수도 있으니까 문자열로 저장
-                newBoard[ny][nx] += board[r][c];
+                // 충돌은 두 구슬이 이동 후 같은 위치에 있는 경우에만 일어난다.
+                newBoard[ny][nx] += ch;
             }
         }
 
@@ -66,15 +68,11 @@ for (let i = 0; i < t; i++) {
             }
         }
 
-        // 구슬이 부딪히지 않았다고 해도 나중에 부딪힐 수 있다.
-        if (!isBlocked) {
-            time++;
-        } else {
-            // 부딪혔다면 시간을 초기화해서 더 오래 기다려본다.
-            time = 0;
-        }
-        result[i] = board.flat().filter(Boolean).length;
+        if (isBlocked) time = 0;
+        else time++;
     }
+    // 남은 구슬의 갯수를 센다
+    result[i] = board.flat().filter(Boolean).length;
 }
 
 console.log(result.join("\n"));
