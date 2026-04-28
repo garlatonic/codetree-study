29d630a4749d685c722675ed4c8b84fe7dfba3d2
IVORY <62371174+garlatonic@users.noreply.github.com>
2026-04-28 14:01:01 +0900

    Title: 겹치지 않게 선분 고르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260428/겹치지 않게 선분 고르기/select-segments-without-overlap.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const segments = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+const MAX_POS = 1000;
+let maxCount = 0;
+const visited = Array(n).fill(false);
+function dfs(index) {
+    if (index === n) {
+        const result = solution(visited);
+        maxCount = Math.max(maxCount, result);
+        return;
+    }
+
+    visited[index] = false;
+    dfs(index + 1);
+    visited[index] = true;
+    dfs(index + 1);
+}
+
+dfs(0);
+console.log(maxCount);
+
+function solution(arr) {
+    let count = 0;
+    const pos = Array(MAX_POS).fill(false);
+
+    for (let i = 0; i < n; i++) {
+        if (!arr[i]) continue;
+
+        const [l, r] = segments[i];
+        for (let p = l; p < r; p++) {
+            if (pos[p]) return 0;
+
+            pos[p] = true;
+        }
+        count += 1;
+    }
+
+    return count;
+}
